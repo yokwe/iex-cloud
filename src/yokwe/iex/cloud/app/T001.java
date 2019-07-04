629e7989e104a7d899598b9cf9d1f6880cedb3bf
hasegawa <hasegawa@ubuntudev.local>
2019-07-04 18:23:14 +0900

    Add more files.

---
+++ src/yokwe/iex/cloud/app/T001.java
@@ -0,0 +1,12 @@
+package yokwe.iex.cloud.app;
+
+import org.slf4j.LoggerFactory;
+
+public class T001 {
+	static final org.slf4j.Logger logger = LoggerFactory.getLogger(T001.class);
+
+	public static void main(String[] args) {
+		logger.info("START");
+		logger.info("STOP");
+	}
+}
