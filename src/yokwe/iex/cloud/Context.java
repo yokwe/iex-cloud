6b50e0061aabb7d922c83eb23765698575a23a50
hasegawa <hasegawa@ubuntudev.local>
2019-07-07 16:31:00 +0900

    Reorganize method getURL()

--- src/yokwe/iex/cloud/Context.java
+++ src/yokwe/iex/cloud/Context.java
@@ -1,11 +1,11 @@
 package yokwe.iex.cloud;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.util.Map;
 
 import org.slf4j.LoggerFactory;
 
 import yokwe.iex.UnexpectedException;
+import yokwe.iex.cloud.Base.Format;
 
 public class Context {
 	static final org.slf4j.Logger logger = LoggerFactory.getLogger(Context.class);
@@ -25,32 +25,28 @@ public class Context {
 		return String.format("[%s %s %s]", type.toString(), version.toString(), url);
 	}
 	
-	public static String encodeSymbol(String symbol) {
-		try {
-			String ret = URLEncoder.encode(symbol, "UTF-8");
-			return ret;
-		} catch (UnsupportedEncodingException e) {
-			String exceptionName = e.getClass().getSimpleName();
-			logger.error("{} {}", exceptionName, e);
-			throw new UnexpectedException(exceptionName, e);
+	public String getBaseURL(String endPoint) {
+		return String.format("%s%s", url, endPoint);
+	}
+	
+	public String getURL(String base) {
+		return String.format("%s?token=%s", base, type.token.secret);
+	}
+	public String getURL(String base, Format format) {
+		return String.format("%s?token=%s&format=%s", base, type.token.secret, format.value);
+	}
+	public String getURL(String base, Format format, Map<String, String> paramMap) {
+		// Sanity check
+		if (paramMap == null) {
+			logger.error("paramMap == null");
+			throw new UnexpectedException("paramMap == null");
 		}
-	}
-	public static String[] encodeSymbol(String[] symbols) {
-		String[] ret = new String[symbols.length];
-		for(int i = 0; i < symbols.length; i++) {
-			ret[i] = encodeSymbol(symbols[i]);
+		
+		StringBuilder ret = new StringBuilder(getURL(base, format));
+		
+		for(Map.Entry<String, String> entry: paramMap.entrySet()) {
+			ret.append(String.format("&%s=%s", Base.encodeString(entry.getKey()), Base.encodeString(entry.getValue())));
 		}
-		return ret;
-	}
-
-
-	public String getURL(String method) {
-		return String.format("%s%s?token=%s", url, method, type.token.secret);
-	}
-	public String getURLAsCSV(String method) {
-		return String.format("%s%s?token=%s&format=csv", url, method, type.token.secret);
-	}
-	public String getURLAsCSV(String method, String symbol) {
-		return String.format("%s%s/%s?token=%s&format=csv", url, method, encodeSymbol(symbol), type.token.secret);
+		return ret.toString();
 	}
 }
