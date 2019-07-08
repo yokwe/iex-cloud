b7772aaa3930d8ada07d34e9634f9ccff0e354b8
hasegawa <hasegawa@ubuntudev.local>
2019-07-08 17:07:03 +0900

    Use SANDBOX for development.

--- src/yokwe/iex/cloud/app/T001.java
+++ src/yokwe/iex/cloud/app/T001.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.slf4j.LoggerFactory;
 
+import yokwe.iex.cloud.Base;
 import yokwe.iex.cloud.Context;
 import yokwe.iex.cloud.Type;
 import yokwe.iex.cloud.Version;
 import yokwe.iex.cloud.data.Status;
 import yokwe.iex.cloud.data.account.Metadata;
 import yokwe.iex.cloud.data.account.Usage;
+import yokwe.iex.util.HttpUtil;
 
 public class T001 {
 	static final org.slf4j.Logger logger = LoggerFactory.getLogger(T001.class);
@@ -17,7 +19,9 @@ public class T001 {
 	public static void main(String[] args) {
 		logger.info("START");
 		
-		Context context = new Context(Type.PRODUCTION, Version.V1);
+		HttpUtil.enableSaveResult(true);
+		
+		Context context = new Context(Type.SANDBOX, Version.V1);
 		logger.info("context = {}", context);
 		
 		{
@@ -35,20 +39,20 @@ public class T001 {
 			logger.info("usage = {}", usage);
 		}
 		
-//		{
-//			List<yokwe.iex.cloud.data.reference.iex.Symbols> iexSymbols = yokwe.iex.cloud.data.reference.iex.Symbols.getInstance(context);
-//			logger.info("iex csv symbols = {}", iexSymbols.size());
-//			
-//			Base.saveCSV(iexSymbols);
-//		}
+		{
+			List<yokwe.iex.cloud.data.reference.iex.Symbols> iexSymbols = yokwe.iex.cloud.data.reference.iex.Symbols.getInstance(context);
+			logger.info("iex csv symbols = {}", iexSymbols.size());
+			
+			Base.saveCSV(iexSymbols);
+		}
 
-//		{
-//          // 100 per call
-//			List<yokwe.iex.cloud.data.reference.Symbols> symbols = yokwe.iex.cloud.data.reference.Symbols.getInstance(context);
-//			logger.info("symbols = {}", symbols.size());
-//			
-//			Base.saveCSV(symbols);
-//		}
+		{
+          // 100 per call
+			List<yokwe.iex.cloud.data.reference.Symbols> symbols = yokwe.iex.cloud.data.reference.Symbols.getInstance(context);
+			logger.info("symbols = {}", symbols.size());
+			
+			Base.saveCSV(symbols);
+		}
 
 //		{
 //			// 100 per call
@@ -87,19 +91,19 @@ public class T001 {
 			String symbol = "IBKR";
 			List<yokwe.iex.cloud.data.dataPoints.Key> keyList = yokwe.iex.cloud.data.dataPoints.Key.getInstance(context, symbol);
 			logger.info("keyList {} = {}", symbol, keyList.size());
-//			for(int i = 0; i < keyList.size(); i++) {
+			for(int i = 0; i < keyList.size(); i++) {
 //				yokwe.iex.cloud.data.dataPoints.Key key = keyList.get(i); 
 //				logger.info("{} {}", symbol, String.format("%3d  %-40s  %4d  %s", i, key.key, key.weight, key.description));
-//			}
+			}
 		}
 		
 		{
 			List<yokwe.iex.cloud.data.timeSeries.Key> keyList = yokwe.iex.cloud.data.timeSeries.Key.getInstance(context);
 			logger.info("keyList = {}", keyList.size());
-//			for(int i = 0; i < keyList.size(); i++) {
+			for(int i = 0; i < keyList.size(); i++) {
 //				yokwe.iex.cloud.data.timeSeries.Key key = keyList.get(i); 
 //				logger.info("{}", key);
-//			}
+			}
 		}
 
 //		{
