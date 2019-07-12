af482fc065e541237fbc339027d6dcf8fdbd6065
hasegawa <hasegawa@ubuntudev.local>
2019-07-12 13:21:41 +0900

    Add method Version.toVersion(String)

--- src/yokwe/iex/cloud/Version.java
+++ src/yokwe/iex/cloud/Version.java
@@ -1,5 +1,12 @@
 package yokwe.iex.cloud;
 
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.slf4j.LoggerFactory;
+
+import yokwe.iex.UnexpectedException;
+
 public enum Version {
 	V1    ("v1"),
 	BETA  ("beta"),
@@ -15,5 +22,23 @@ public enum Version {
 	@Override
 	public String toString() {
 		return String.format("%s", name());
+	}
+	
+	
+	static final org.slf4j.Logger logger = LoggerFactory.getLogger(Version.class);
+
+	private static Map<String, Version> versionMap = new TreeMap<>();
+	static {
+		for(Version type: Version.class.getEnumConstants()) {
+			versionMap.put(type.name(), type);
+		}
+	}
+	public static Version toVersion(String value) {
+		if (versionMap.containsKey(value)) {
+			return versionMap.get(value);
+		} else {
+			logger.error("Unexpected value  {}", value);
+			throw new UnexpectedException("Unexpected value");
+		}
 	}
 }
