5b9296edc153d9390a80cadfe85d02315857c47c
hasegawa <hasegawa@ubuntudev.local>
2019-07-12 13:21:12 +0900

    Add method Type.toType(String)

--- src/yokwe/iex/cloud/Type.java
+++ src/yokwe/iex/cloud/Type.java
@@ -1,5 +1,12 @@
 package yokwe.iex.cloud;
 
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.slf4j.LoggerFactory;
+
+import yokwe.iex.UnexpectedException;
+
 public enum Type {
 	PRODUCTION(Token.PRODUCTION, "https://cloud.iexapis.com"),
 	SANDBOX   (Token.SANDBOX,    "https://sandbox.iexapis.com");
@@ -15,5 +22,23 @@ public enum Type {
 	@Override
 	public String toString() {
 		return String.format("%s", this.name());
+	}
+	
+	
+	static final org.slf4j.Logger logger = LoggerFactory.getLogger(Type.class);
+
+	private static Map<String, Type> typeMap = new TreeMap<>();
+	static {
+		for(Type type: Type.class.getEnumConstants()) {
+			typeMap.put(type.name(), type);
+		}
+	}
+	public static Type toType(String value) {
+		if (typeMap.containsKey(value)) {
+			return typeMap.get(value);
+		} else {
+			logger.error("Unexpected value  {}", value);
+			throw new UnexpectedException("Unexpected value");
+		}
 	}
 }
